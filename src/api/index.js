abaa25dfae095d79a273846da5308e2e25efa50c
Rob Kritzinger <kritz.rob@gmail.com>
2022-05-29 09:51:55 +0200

    more naming

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth.mw')
 const unless = require('../util').unless
 
 router.use(express.json())
@@ -10,10 +10,10 @@ router.use((req, res, next) => {
     next();
 });
 
-router.use('/users', require('./controllers/users'));
-router.use('/posts', require('./controllers/posts'));
-router.use('/comments', require('./controllers/comments'));
-router.use('/follows', require('./controllers/follows'));
-router.use('/auth', require('./controllers/auth'));
+router.use('/users', require('./controllers/users.controller'));
+router.use('/posts', require('./controllers/posts.controller'));
+router.use('/comments', require('./controllers/comments.controller'));
+router.use('/follows', require('./controllers/follows.controller'));
+router.use('/auth', require('./controllers/auth.controller'));
 
 module.exports = router
